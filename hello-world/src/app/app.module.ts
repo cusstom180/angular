9daaab8c608185bb16cffd304f06497d51a22ebc
cusstom180 <cusstom180@icloud.com>
2020-01-16 22:26:49 -0500

    first commit

---
+++ hello-world/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { CoursesService } from './course/courses.service';
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    CoursesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
